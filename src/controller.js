324c8b648fbb64b2a9f9de2721fc27b62b95ae67
Cristina Iannone <iannonecristina@gmail.com>
2017-05-26 12:59:33 +0100

    Adds make request logic to controller.js

--- src/controller.js
+++ src/controller.js
@@ -1,17 +1,36 @@
-// (function(exports) {
-//   var Controller = function() {
-//       this.articleNews = new Article(article);
-//       this.listHeadlines = new HeadLines(this.articleNews);
-//   };
-//
-//   // Controller.prototype.changeGretting = function() {
-//   //     document.getElementById("app").innerHTML = "Hi Folks";
-//   // };
-//
-//   Controller.prototype.insertHTML = function() {
-//       document.getElementById('headlines').innerHTML = this.displayHeadlines();
-//   };
-//
-//
-//   exports.Controller =  Controller;
-// })(this);
+/* (function(exports) {
+   var Controller = function() {
+       this.articleNews = new Article(article);
+       this.listHeadlines = new HeadLines(this.articleNews);
+   };
+*/
+//var bustCache = '?' + new Date().getTime();
+//oReq.open('GET', e.target.dataset.url + bustCache, true);
+
+(function() {
+
+  function makeRequest() {
+  httpRequest = new XMLHttpRequest();
+  var key = "../key.txt";
+
+  if (!httpRequest) {
+    alert('Giving up :( Cannot create an XMLHTTP instance');
+    return false;
+  }
+  httpRequest.onreadystatechange = alertContents;
+  httpRequest.open('GET', "https://content.guardianapis.com/search?api-key=" + key);
+  httpRequest.send();
+}
+
+function alertContents() {
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      console.log(httpRequest.status);
+      alert(httpRequest.responseText);
+    } else {
+      alert('There was a problem with the request.');
+    }
+  }
+}
+
+})();
--- src/guardian-api.js
+++ src/guardian-api.js
@@ -1,16 +1,18 @@
-// (function(exports) {
-//
-//   function GuardianApiRequest() {
-//     this.makersApi = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=";
-//     this.articlesUrl = "http://content.guardianapis.com/search?from-date=";
-//     this.date = new Date();
-//     this.dateString = this.date.toISOString().split('T')[0];
-//   };
-//
-//   GuardianApiRequest.prototype.createRequestUrl = function() {
-//     return this.makersApi + this.articlesUrl + this.dateString;
-//   };
-//
-//   exports.GuardianApiRequest = GuardianApiRequest;
-//
-// })(this);
+/*
+ (function(exports) {
+
+   function GuardianApiRequest() {
+     this.makersApi = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=";
+     this.articlesUrl = "http://content.guardianapis.com/search?from-date=";
+    this.date = new Date();
+    this.dateString = this.date.toISOString().split('T')[0];
+   };
+
+   GuardianApiRequest.prototype.createRequestUrl = function() {
+     return this.makersApi + this.articlesUrl + this.dateString;
+   };
+
+   exports.GuardianApiRequest = GuardianApiRequest;
+
+ })(this);
+ */
