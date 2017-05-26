fd3f279406a405001a76f0354940812cd5ae90ab
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-05-26 14:35:59 +0100

    adds guardianAPI request to index.html

--- src/controller.js
+++ src/controller.js
@@ -7,30 +7,31 @@
 //var bustCache = '?' + new Date().getTime();
 //oReq.open('GET', e.target.dataset.url + bustCache, true);
 
-(function() {
-
-  function makeRequest() {
-  httpRequest = new XMLHttpRequest();
-  var key = "../key.txt";
-
-  if (!httpRequest) {
-    alert('Giving up :( Cannot create an XMLHTTP instance');
-    return false;
-  }
-  httpRequest.onreadystatechange = alertContents;
-  httpRequest.open('GET', "https://content.guardianapis.com/search?api-key=" + key);
-  httpRequest.send();
-}
-
-function alertContents() {
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-    if (httpRequest.status === 200) {
-      console.log(httpRequest.status);
-      alert(httpRequest.responseText);
-    } else {
-      alert('There was a problem with the request.');
-    }
-  }
-}
-
-})();
+// (function() {
+//   var httpRequest;
+//
+//   function makeRequest() {
+//   httpRequest = new XMLHttpRequest();
+//   var key = "../key.txt";
+//
+//     if (!httpRequest) {
+//       alert('Giving up :( Cannot create an XMLHTTP instance');
+//       return false;
+//     }
+//     httpRequest.onreadystatechange = alertContents;
+//     httpRequest.open('GET', "https://content.guardianapis.com/search?api-key=" + key);
+//     httpRequest.send();
+//   }
+//
+//   function alertContents() {
+//     if (httpRequest.readyState === XMLHttpRequest.DONE) {
+//       if (httpRequest.status === 200) {
+//         console.log("test2");
+//         alert(httpRequest.responseText);
+//       } else {
+//         alert('There was a problem with the request.');
+//       }
+//     }
+//   }
+//
+// })();
