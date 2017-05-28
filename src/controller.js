bacb5326243af324c6a464033b99b5faba83e065
Cristina Iannone <iannonecristina@gmail.com>
2017-05-28 15:25:56 +0100

    Lists the articles' headlines as an ordered list

--- src/controller.js
+++ src/controller.js
@@ -1,37 +1,40 @@
-/* (function(exports) {
-   var Controller = function() {
-       this.articleNews = new Article(article);
-       this.listHeadlines = new HeadLines(this.articleNews);
-   };
-*/
-//var bustCache = '?' + new Date().getTime();
-//oReq.open('GET', e.target.dataset.url + bustCache, true);
+(function() {
+  var httpRequest;
+  document.getElementById("ajaxButton").addEventListener('click', makeRequest);
 
-// (function() {
-//   var httpRequest;
-//
-//   function makeRequest() {
-//   httpRequest = new XMLHttpRequest();
-//   var key = "../key.txt";
-//
-//     if (!httpRequest) {
-//       alert('Giving up :( Cannot create an XMLHTTP instance');
-//       return false;
-//     }
-//     httpRequest.onreadystatechange = alertContents;
-//     httpRequest.open('GET', "https://content.guardianapis.com/search?api-key=" + key);
-//     httpRequest.send();
-//   }
-//
-//   function alertContents() {
-//     if (httpRequest.readyState === XMLHttpRequest.DONE) {
-//       if (httpRequest.status === 200) {
-//         console.log("test2");
-//         alert(httpRequest.responseText);
-//       } else {
-//         alert('There was a problem with the request.');
-//       }
-//     }
-//   }
-//
-// })();
+  function makeRequest() {
+  httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', "https://content.guardianapis.com/search?api-key=" + guardianApiKey);
+    httpRequest.send();
+  }
+
+  function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        var guardian = JSON.parse(httpRequest.responseText);
+        console.log(guardian);
+        var articles = guardian.response.results;
+        console.log(articles);
+        parent = document.getElementById('app');
+        for (var i = 0; i < articles.length; i++) {
+          var article = document.createElement("div");
+          article.innerHTML = "<li>" + articles[i].webTitle + "</li>" + "<br>";
+          parent.appendChild(article);
+      }
+
+      var button = document.getElementById('ajaxButton');
+      button.style.visibility = "hidden";
+
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+
+})();
